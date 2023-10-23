9482c80e02d09820693284cc09c0c71b906b016d
David Stewart <davidd.cc.stewartt@gmail.com>
2023-10-23 14:40:15 +0100

    Added: Basic Styling

--- src/Screens/SelectGameMode.tsx
+++ src/Screens/SelectGameMode.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
-
+import { GoNumber } from 'react-icons/go';
+import { RiComputerFill } from 'react-icons/ri';
 import Box from '../Components/Box';
 
 type Props = {}
@@ -14,7 +15,12 @@ const SelectGameMode = (props: Props) => {
             <h1>Guessing Game</h1>
 			<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', margin: '20px 0'}}>
 				<Box highlight={gameModeId === 1} onClick={() => setGameModeId(1)}>
-					<p>Have a computer guess your number</p>
+					<GoNumber color={gameModeId === 1 ? '#268a8a' : '#2dc8c8'} size={70} />
+					<p>You think of a number, and have a computer guess your number</p>
+				</Box>
+				<Box highlight={gameModeId === 2} onClick={() => setGameModeId(2)}>
+					<RiComputerFill color={gameModeId === 2 ? '#268a8a' : '#2dc8c8'} size={70} />
+					<p>You, the player, guesses the computer's number</p>
 				</Box>
 			</div>
 			
