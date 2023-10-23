5b321ed0715cee22044e143b7272243ea0c84b6e
David Stewart <davidd.cc.stewartt@gmail.com>
2023-10-23 16:07:01 +0100

    Updated: Success screen to be dynamic

--- src/Components/Success.tsx
+++ src/Components/Success.tsx
@@ -2,22 +2,27 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 type SuccessProps = {
-    numberOfAttempts: number
+    numberOfAttempts: number;
+    gamemode?: number;
 }
 
-const Success: React.FC<SuccessProps> = ({ numberOfAttempts }) => {
+const Success: React.FC<SuccessProps> = ({ numberOfAttempts, gamemode }) => {
     const navigate = useNavigate();
 
     return (
         <div>
             <h1>Success!</h1>
-            <p>It took the computer {numberOfAttempts} attempts to guess your number.</p>
+            <p>It took {gamemode === 1 ? 'the computer' : 'you'} {numberOfAttempts} attempts to guess {gamemode === 1 ? 'your' : 'the computers'} number.</p>
             <div>
                 <button className='m0-10' onClick={() => window.location.reload()}>Play Again</button>
                 <button className='m0-10' onClick={() => navigate("/")}>Select a different mode</button>
             </div>
         </div>
     )
+}
+
+Success.defaultProps = {
+    gamemode: 1
 }
 
 export default Success
